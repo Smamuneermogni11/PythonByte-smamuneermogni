7c5d885300c6d8c7fb54d4de2880050ce7aa786e
smamuneermogni <mohammad-abdul-muneer-mogni.shaik@outlook.com>
2022-10-01 17:16:04 -0700

    test

--- test/test_front_page.py
+++ test/test_front_page.py
@@ -1,3 +1,8 @@
-def test_front_page(client):
-    response = client.get("/posts")
-    assert b"<h2>PythonByte!</h2>" in response.data
+# test_hello.py
+from app import app
+
+def test_webpage():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data == b'Hello, World!'
